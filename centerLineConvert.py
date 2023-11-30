92a4f924fc7e8b5b70286e3764b1e5a3cb909cb5
JieWang <50101921+JieWangnk@users.noreply.github.com>
2023-11-30 09:39:35 +0000

    Add files via upload

--- centerLineConvert.py
+++ centerLineConvert.py
@@ -4,35 +4,22 @@ The normal vector field is used to define the slice plane for paraview to slice 
 The normal vector field is saved to a csv file with header x,y,z,nx,ny,nz
 input: centerLinePlaque.csv
 output: centerLinePlaqueNormal.csv
-we use the extrapolated points to calculate the normal vectors, here we use 2 extrapolated points between each two points
 
 usage: python centerLineConvert.py
 """
 
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Read data from csv file
 dataPoint = np.genfromtxt('centerLinePlaque.csv', delimiter=',', skip_header=1)
 
-# Number of extrapolated points
-num_points = 2
-# Extrapolate points
-extrapolated_points = []
-for i in range(len(dataPoint) - 1):
-    for j in range(num_points):
-        alpha = j / (num_points - 1)
-        interpolated_point = (1 - alpha) * \
-            dataPoint[i] + alpha * dataPoint[i + 1]
-        extrapolated_points.append(interpolated_point)
-
-extrapolated_points = np.array(extrapolated_points)
+dataPoint = np.array(dataPoint)
 
 # Calculate normal vectors between points
 normal_vectors = [[0, 0, 0]]
-for i in range(1, len(extrapolated_points)):
-    vector = extrapolated_points[i] - extrapolated_points[i - 1]
+for i in range(1, len(dataPoint)):
+    vector = dataPoint[i] - dataPoint[i - 1]
     norm = np.linalg.norm(vector)
     if norm > 0:
         normal_vector = vector / norm
@@ -44,9 +31,8 @@ for i in range(1, len(extrapolated_points)):
 
 normal_vectors = np.array(normal_vectors)
 
-print('extrapolated_points.shape = ', extrapolated_points.shape)
 # save normal vectors and extrapolated points to one file with header x,y,z,nx,ny,nz
-data = np.concatenate((extrapolated_points, normal_vectors), axis=1)
+data = np.concatenate((dataPoint, normal_vectors), axis=1)
 np.savetxt('centerLinePlaqueNormal.csv', data, delimiter=',',
            header='x,y,z,nx,ny,nz', comments='')
 
